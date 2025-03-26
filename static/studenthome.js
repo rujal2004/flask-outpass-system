71f4bb133301479b348b260244291b009585d716
rujal2004 <rujalgupta656@gmail.com>
2025-03-26 19:07:49 +0530

    commit

---
+++ static/studenthome.js
@@ -0,0 +1,22 @@
+function applyForOutpass() {
+    alert("Navigating to Apply for Outpass page...");
+    window.location.href = "/apply_outpass";
+}
+
+function viewPendingOutpasses() {
+    alert("Navigating to View Pending Outpasses page...");
+    window.location.href = "/pending_outpasses";
+}
+
+
+function viewPreviousOutpasses() {
+   
+    alert("Navigating to View Previous Outpasses page...");
+    window.location.href = "/previous_outpasses";
+}
+
+
+function logOut() {
+    alert("Logging out...");
+    window.location.href = "/";
+}
